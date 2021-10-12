d6d9ef274d463165964450c9dcc2557f1791202d
Daniel <d.y.young@outlook.com>
2021-10-12 15:35:59 -0600

    Undo Functionality
    Based on the original command pattern, adding a undo button for undo operation.

--- Assets/Scripts/Commands/MoveCommand.cs
+++ Assets/Scripts/Commands/MoveCommand.cs
@@ -22,4 +22,15 @@ public class MoveCommand : CommandManager.ICommand
             Gameboard.Instance.SwitchTeam();
         }
     }
+
+    public void Undo()
+    {
+        var unit = Gameboard.Instance.GetUnit(m_To);
+        if(unit != null)
+        {
+            Gameboard.Instance.MoveUnit(unit, m_From);
+        }
+        //we switch back which team is playing, as we undid a move, it go back to previous team
+        Gameboard.Instance.SwitchTeam();
+    }
 }
